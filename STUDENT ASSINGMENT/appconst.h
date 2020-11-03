d4f4a7eadf2329b6a2ddeaa7dc487260f93a3950
Duggupriya <73168478+Duggupriya@users.noreply.github.com>
2020-11-03 11:59:58 +0530

    Add files via upload

---
+++ STUDENT ASSINGMENT/appconst.h
@@ -0,0 +1,23 @@
+/*DEFINES ALL THE GLOBAL VARIABLES AND STORING THE VALUES
+
+  Author:              Priyanshu Dugaya
+  Registration number: 201046023
+  Department:          Big data and data analytics
+  Date:                03/11/2020
+
+*/
+
+#ifndef APPCONST_H_INCLUDED
+#define APPCONST_H_INCLUDED
+
+#define STUDENT_NAME_LEN   64
+
+#define PROGRAM_NAME_LEN   32
+#define STUDENT_EMAIL_LEN  64
+#define TOTAL_STUDENT     100
+#define STATUS_LEN         20
+
+#endif
+
+
+
